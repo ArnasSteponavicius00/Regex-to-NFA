74f551a1bf0605824fdb0e1c141ece2bf45867b0
Arnas Steponavicius <g00361891@gmit.ie>
2020-01-16 20:47:58 +0000

    Started a very simple program to just print out notes I could gather on a topic. Mainly to familiarise myself with using VI as an editor and learn something in the process.

---
+++ study_research/automataNotes.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+## Sources: Dr.Ian McLoughlin, Wikipedia, DigiiMento(YouTube Channel)
+
+## Notes on Automatons.
+## Using VI and Python to print notes relevant to college module, So I can familiarise myself with simple tasks when using a console to code and hopefully absorb some knowledge in the process :).
+
+## Arnas Steponavicius - G00361891 - Graph Theory
+
+print("\n")
+
+## Source: Wikipedia on Automata Theory.
+print("Automata:\n")
+print("Automaton is a self-operating machine designed to follow a determined sequence of operations.\n")
+print("Runs when given a sequence of inputs (Individual time step or steps). Processes one input from a set of [SYMBOLS] or [LETTERS] called an [ALPHABET]")
+print("A finite sequence of these [SYMBOLS] are known as [WORDS], received by the automaton(machine) at any step.")
+print("Automata have a finite set of states, while the Automaton is running, it is always in one of those states.")
+print("When an input is received, it transitions to another state based on a function that takes into account\n1. Current State\n2. [SYMBOLS] as a parameter.\nThis is known as a transition function.")
+print("The Automaton reads in the [SYMBOLS] of a [WORD] and moves from state to state until the [WORD] is completely read, where it stops running, this is the final state.")
+print("Based on this final state, the Automaton either accepts or rejects the input [WORD]")
+print("The set of [WORDS] that is accepted by the automaton is a [LANGUAGE]")
+
+
+## Source: DigiiMento (YouTube)
+print("\n\n")
+print("Types of Automata (machines)")
+print("Finite Automatons / State Machines = Mathematical Model of Computation (MMC)")
+print("-------------------------------------------------------------------------")
+print("MMC is just a function that computes an output when given an input.")
+print("2 Types of Finite Automata:\n-Deterministic Finite Automata\n-Non-Deterministic Finite Automata")
+
+
+print("\n\n")
+
